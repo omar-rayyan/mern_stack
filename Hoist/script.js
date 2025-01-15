a6881a0e4c64e77503c0882aa81277b024a8aff2
omar-rayyan <omarrayyan999@gmail.com>
2025-01-15 13:21:14 +0200

    Hoist assignment done.

---
+++ Hoist/script.js
@@ -0,0 +1,145 @@
+// Example 1
+console.log(hello);
+var hello = 'world';
+
+// After hoisting:
+var hello;
+console.log(hello); // outputs undefined
+hello = 'world';
+
+
+
+// Example 2
+var needle = 'haystack';
+test();
+function test() {
+    var needle = 'magnet';
+    console.log(needle);
+}
+
+// After hoisting:
+var needle;
+function test() {
+    var needle
+    needle = 'magnet';
+    console.log(needle); // outputs 'magnet'
+}
+needle = 'haystack';
+test();
+
+
+
+// Example 3
+var brendan = 'super cool';
+function print() {
+    brendan = 'only okay';
+    console.log(brendan);
+}
+console.log(brendan);
+
+// After hoisting:
+var brendan;
+function print() {
+    brendan = 'only okay';
+    console.log(brendan);
+}
+brendan = 'super cool';
+console.log(brendan); // outputs 'super cool'
+
+
+
+// Example 4
+var food = 'chicken';
+console.log(food);
+eat();
+function eat() {
+    var food = 'half-chicken';
+    console.log(food);
+    food = 'gone';
+}
+
+// After hoisting:
+var food;
+function eat() {
+    var food;
+    food = 'half-chicken';
+    console.log(food); // outputs 'half-chicken'
+    food = 'gone';
+}
+food = 'chicken';
+console.log(food); // outputs 'chicken'
+eat();
+
+
+
+// Example 5
+mean();
+console.log(food);
+var mean = function() {
+    var food = "chicken";
+    console.log(food);
+    food = "fish";
+    console.log(food);
+};
+console.log(food);
+
+// After hoisting:
+var mean;
+console.log(food); // outputs undefined
+mean(); // Outputs an error because mean is not a function
+
+
+
+// Example 6
+console.log(genre);
+var genre = "disco";
+rewind();
+function rewind() {
+    var genre = "rock";
+    console.log(genre);
+    genre = "r&b";
+    console.log(genre);
+}
+console.log(genre);
+
+// After hoisting:
+var genre;
+function rewind() {
+    var genre;
+    genre = "rock";
+    console.log(genre); // outputs 'rock'
+    genre = "r&b";
+    console.log(genre); // outputs 'r&b'
+}
+console.log(genre); // outputs undefined
+genre = "disco";
+rewind();
+console.log(genre); // outputs 'disco'
+
+
+
+// Example 7
+dojo = "san jose";
+console.log(dojo);
+learn();
+function learn() {
+    var dojo = "seattle";
+    console.log(dojo);
+    dojo = "burbank";
+    console.log(dojo);
+}
+console.log(dojo);
+
+// After hoisting:
+var dojo;
+function learn() {
+    var dojo;
+    dojo = "seattle";
+    console.log(dojo); // outputs 'seattle'
+    dojo = "burbank";
+    console.log(dojo); // outputs 'burbank'
+}
+dojo = "san jose";
+console.log(dojo); // outputs 'san jose'
+learn();
+console.log(dojo); // outputs 'san jose'
